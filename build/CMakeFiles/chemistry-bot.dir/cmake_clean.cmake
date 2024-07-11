ebbc10b896a0b11bab4880a572d0236d0687cc0c
Rahul Saini <rahul.saini@education.nsw.gov.au>
2024-07-11 21:21:48 +1000

    was an env issue builds now

---
+++ build/CMakeFiles/chemistry-bot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chemistry-bot.dir/src/main.cpp.o"
+  "CMakeFiles/chemistry-bot.dir/src/main.cpp.o.d"
+  "chemistry-bot"
+  "chemistry-bot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chemistry-bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/chemistry-bot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for chemistry-bot.
+# This may be replaced when dependencies are built.
